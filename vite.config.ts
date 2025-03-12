e419158ed456e1897fe9bcb463d8c80cefd91972
u1_mas <mtakuya1022@gmail.com>
2025-03-12 15:48:15 +0900

    [style] TailwindCSSの設定修正 (from cline)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { Mode, plugin as markdown } from "vite-plugin-markdown";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         react(),
         markdown({
             mode: [Mode.REACT],
