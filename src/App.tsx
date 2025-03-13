57a5e5639270096a44a3ee5f2e27108825675d52
u1_mas <mtakuya1022@gmail.com>
2025-03-13 12:54:09 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AppRoutes } from "./routes"
 import type { BlogPost } from "./types/BlogPost"
 
 function App() {
-    const [contents] = useState<Record<string, number>>(loadContent())
+    const [contents] = useState<Record<string, BlogPost>>(loadContent())
     return <AppRoutes contents={contents} />
 }
 
